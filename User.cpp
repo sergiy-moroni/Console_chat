8580c04c4f940762d821f7e2845a16ab576c6b3b
sergiy-moroni <90573461+sergiy-moroni@users.noreply.github.com>
2022-11-26 14:42:27 +0300

    Add files via upload

---
+++ User.cpp
@@ -0,0 +1,35 @@
+﻿#include "User.h"
+
+User::User(string name, string login, string pass) {
+	_name = name;
+	_login = login;
+	_pass = pass;
+}
+
+User::~User() {}
+string User::GetName() {
+	return _name;
+}
+void User::SetName(string name) {
+	_name = name;
+}
+string User::Getpass() {
+	return _pass;
+}
+string User::Getlog() {
+	return _login;
+}
+
+//получение сообщения 
+void User::Setmessage(Message<string> message) {
+
+	_messages.push_back(message);
+}
+
+// чтение сообщений 
+void User::PrintMessages() {
+	for (int i = 0; i < size(_messages); ++i) {
+		_messages[i].PrintM();
+	}
+	cout << endl;
+}
---
+++ User.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Message.h"
+
+using namespace std;
+class User {
+
+private:
+	string _name;
+	string _pass;
+	string _login;
+	vector<Message<string>> _messages;
+
+public:
+	User(string name, string login, string pass);
+	~User();
+	string GetName();
+	void SetName(string name);
+	string Getpass();
+	string Getlog();
+
+	//получение сообщения 
+	void Setmessage(Message<string> message);
+
+	// чтение сообщений 
+	void PrintMessages();
+};
+
+
